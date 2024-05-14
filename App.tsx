b173bf709cc61d5892b75c3d6689ff6bd9a6d74e
Wilso0nDeV <kaphy.dev@gmail.com>
2024-05-13 19:04:19 -0500

    counterapp react-native

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ export const App = () => {
   
   return (
 
-    //  Configuración para usar la liberóa de react-native-paper y los iconos de Ionic 
+    //  Configuración para usar la librería de react-native-paper y los iconos de Ionic 
 
     <PaperProvider
       settings={{
